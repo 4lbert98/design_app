9ca7528e18a3728e2a886c286cf52437e2ec5273
4lbert98 <65789582+4lbert98@users.noreply.github.com>
2020-05-24 16:03:53 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:design_app/Login_Page.dart';
+import 'package:design_app/Place_page.dart';
+import 'package:design_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  title: "Design Flutter App",
+  home: LoginPage(),
+  )
+);
+
